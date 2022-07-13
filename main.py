94691d4ea68bd3a73d60b1e650f7966400b51382
Leonard Blam <blam@g.jct.ac.il>
2022-07-13 16:22:06 +0300

    improvement with flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,5 +11,31 @@ def index():
 @app.route("/books")
 def books():
     return render_template("Books.html")
+
+@app.route("/movies")
+def movies():
+    return render_template("Movies.html")
+
+@app.route("/series")
+def series():
+    return render_template("Series.html")
+
+@app.route("/project")
+def project():
+    return render_template("Project.html")
+
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("Terms&Conditions.html")
+
+
 
 app.run(debug=True)
