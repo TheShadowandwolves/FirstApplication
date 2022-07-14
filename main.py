210170b47a856af145b3c745071bf77f14444c61
Leonard Blam <blam@g.jct.ac.il>
2022-07-14 15:52:49 +0300

    eachmovie shows particular name

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template, url_for
+from email.mime import image
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+class movie:
+    title = "amb"
+    qualitiy = "1080p"
+    rating = 5
+    publishdate = 2020
+    length = 60
+    language = ["german", "english", "french"]
+    category = ["horror", "comedy"]
+    description = "none"
+    screenshots = []
+    image = "none"
 
-@app.route("/home")
-@app.route("/")
-def index():
-    return render_template("index.html")
+
+def getMovieData(movie):
+    data = movie
+    return data
+
+
+@app.route("/home/<name>")
+@app.route("/<name>")
+def index(name):
+    mylist = [1,2,3,4,5]
+
+    return render_template("index.html", name=name)
 
 @app.route("/books")
 def books():
@@ -36,9 +56,10 @@ def login():
 def terms():
     return render_template("Terms&Conditions.html")
 
-@app.route("/eachMovie")
-def eachMovie():
-    return render_template("EachMovie.html")
+@app.route("/eachMovie/<movietitle>")
+def eachMovie(movietitle):
+    movie.title = movietitle
+    return render_template("EachMovie.html", movie = movie)
 
 @app.route("/ex")
 def ex():
